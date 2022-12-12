1ebff54227cb260b6a35f4ce7aefb56ee669d796
Kevin Brasier <wonderfulweb@gmail.com>
2022-12-12 08:09:08 -0800

    rustfmt

--- 2022/02-rock-paper-scissors/src/main.rs
+++ 2022/02-rock-paper-scissors/src/main.rs
@@ -5,9 +5,9 @@ use std::process;
 /// Calculate total score from following strategy guide file
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args{
+struct Args {
     /// The file to use as input
-    input: PathBuf
+    input: PathBuf,
 }
 
 fn main() {
