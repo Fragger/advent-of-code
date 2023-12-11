80c6d8fdaf9f0c7cb225d278d55e1976779fdc58
Kevin Brasier <wonderfulweb@gmail.com>
2023-12-11 03:00:26 -0800

    Use accumulate instead of <, > comparisons day 11

--- 2023/solution/11.py
+++ 2023/solution/11.py
@@ -1,22 +1,21 @@
 import sys
-from itertools import combinations
+from itertools import accumulate, combinations
 
 with open(sys.argv[1] if len(sys.argv) > 1 else '../input/11') as f:
     image = f.read().splitlines()
 
-rows = [r for r, row in enumerate(image) if '#' not in row]
-cols = [c for c, col in enumerate(zip(*image)) if '#' not in col]
+rows = list(accumulate('#' not in row for row in image))
+cols = list(accumulate('#' not in col for col in zip(*image)))
 
-galaxys = ((c, r) for r, row in enumerate(image)
+galaxies = ((c, r) for r, row in enumerate(image)
         for c, point in enumerate(row) if point == '#')
 
 total = 0
 expand = 0
-for a, b in combinations(galaxys, 2):
-    min_col, max_col = sorted((a[0], b[0]))
-    min_row, max_row = sorted((a[1], b[1]))
+for (a_col, a_row), (b_col, b_row) in combinations(galaxies, 2):
+    min_col, max_col = sorted((a_col, b_col))
+    min_row, max_row = sorted((a_row, b_row))
     total += max_col - min_col + max_row - min_row
-    expand += sum(min_col < col < max_col for col in cols)
-    expand += sum(min_row < row < max_row for row in rows)
+    expand += cols[max_col] - cols[min_col] + rows[max_row] - rows[min_row]
 print(total + expand)
 print(total + expand*999999)
