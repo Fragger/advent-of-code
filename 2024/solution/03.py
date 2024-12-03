fab2b57d7e35438f03063c5579ce34fe581fe689
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-03 00:01:46 -0800

    Match groups instead of extra string comps day 03

--- 2024/solution/03.py
+++ 2024/solution/03.py
@@ -7,17 +7,17 @@ with open(sys.argv[1] if len(sys.argv) > 1 else '../input/03') as f:
 p1 = 0
 p2 = 0
 mulenabled = True
-for match in re.finditer(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)", memory):
-    instr = match.group(0)
-    if instr.startswith('mul'):
-        x, y = map(int, match.groups())
+for match in re.finditer(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))", memory):
+    *xy, do, dont = match.groups()
+    if do:
+        mulenabled = True
+    elif dont:
+        mulenabled = False
+    else:
+        x, y = map(int, xy)
         p1 += x*y
         if mulenabled:
             p2 += x*y
-    elif instr == 'do()':
-        mulenabled = True
-    elif instr == "don't()":
-        mulenabled = False
 
 print(p1)
 print(p2)
