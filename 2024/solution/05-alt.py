5ed3f2fdf7856c9fc9943084ab90da4f6f534221
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-04 22:45:49 -0800

    Alt solution for 2024 day 05 using native sort with cmp func

---
+++ 2024/solution/05-alt.py
@@ -0,0 +1,28 @@
+import sys
+from collections import defaultdict
+from functools import cmp_to_key
+
+with open(sys.argv[1] if len(sys.argv) > 1 else '../input/05') as f:
+    po, updates = f.read().split('\n\n')
+
+rules = defaultdict(set)
+for rule in po.splitlines():
+    first, second = rule.split('|')
+    rules[first].add(second)
+
+def cmp_rules(a, b):
+    if b not in rules[a]:
+        return 1
+    return -1
+
+p1 = 0
+p2 = 0
+for update in updates.splitlines():
+    pages = update.split(',')
+    sortedpages = sorted(pages, key=cmp_to_key(cmp_rules))
+    if sortedpages == pages:
+        p1 += int(pages[len(pages)//2])
+    else:
+        p2 += int(sortedpages[len(pages)//2])
+print(p1)
+print(p2)
