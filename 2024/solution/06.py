128367dbb51e884f86a2e9303307216916b3fdd1
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-06 04:52:17 -0800

    Clean up some variable names for 2024 day 06

--- 2024/solution/06.py
+++ 2024/solution/06.py
@@ -9,32 +9,32 @@ ylen = len(labmap)
 for y in range(ylen):
     for x in range(xlen):
         if labmap[y][x] == '^':
-            loc = x, y
+            start = x, y
 
 dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))
 
 orent = 0
-start = loc
+x, y = start
 visited = {start}
-while 0 < loc[0] < xlen-1 and 0 < loc[1] < ylen-1:
-    x, y = loc[0]+dirs[orent][0], loc[1]+dirs[orent][1]
-    if labmap[y][x] == '#':
+while 0 < x < xlen-1 and 0 < y < ylen-1:
+    nx, ny = x+dirs[orent][0], y+dirs[orent][1]
+    if labmap[ny][nx] == '#':
         orent = (orent+1)%4
     else:
-        loc = x, y
-        visited.add(loc)
+        x, y = nx, ny
+        visited.add((x, y))
 print(len(visited))
 
 def loop(block):
     orent = 0
-    loc = start
-    visited = {start+(orent,)}
-    while 0 < loc[0] < xlen-1 and 0 < loc[1] < ylen-1:
-        x, y = loc[0]+dirs[orent][0], loc[1]+dirs[orent][1]
-        if labmap[y][x] == '#' or (x, y) == block:
+    x, y = start
+    visited = {(x, y, orent)}
+    while 0 < x < xlen-1 and 0 < y < ylen-1:
+        nx, ny = x+dirs[orent][0], y+dirs[orent][1]
+        if labmap[ny][nx] == '#' or (nx, ny) == block:
             orent = (orent+1)%4
         else:
-            loc = x, y
+            x, y = nx, ny
             visit = x, y, orent
             if visit in visited:
                 return True
