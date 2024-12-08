8684ed058c96bd2176dc4091998bfae8f8a10c73
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-07 20:38:04 -0800

    Change 2024 day 07 to nit some things from initial
    Use add and mul from operator and make concat function, letting the if
    tree be ditched
    Use log10 instead of str conversion for concat

--- 2024/solution/07.py
+++ 2024/solution/07.py
@@ -1,5 +1,10 @@
 import sys
 from itertools import product
+from math import log10
+from operator import add, mul
+
+def concat(a, b):
+    return a*10**(int(log10(b))+1)+b
 
 p1 = 0
 p2 = 0
@@ -8,17 +13,12 @@ with open(sys.argv[1] if len(sys.argv) > 1 else '../input/07') as f:
         test, nums = line.split(':')
         test = int(test)
         nums = tuple(map(int, nums.split()))
-        for ops in product(('+', '*', '||'), repeat=len(nums)-1):
+        for ops in product((add, mul, concat), repeat=len(nums)-1):
             value = nums[0]
-            for i, op in enumerate(ops, 1):
-                if op == '+':
-                    value += nums[i]
-                elif op == '*':
-                    value *= nums[i]
-                elif op == '||':
-                    value = int(str(value)+str(nums[i]))
+            for num, op in zip(nums[1:], ops):
+                value = op(value, num)
             if value == test:
-                if '||' not in ops:
+                if concat not in ops:
                     p1 += test
                 p2 += test
                 break
