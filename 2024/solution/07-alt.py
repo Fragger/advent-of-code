d4903517bdc46276ff81821bab044d682bcaabeb
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-07 20:45:27 -0800

    Use an index instead of slicing on 2024 day 07 alt

--- 2024/solution/07-alt.py
+++ 2024/solution/07-alt.py
@@ -5,12 +5,12 @@ from operator import add, mul
 def concat(a, b):
     return a*10**(int(log10(b))+1)+b
 
-def calibrate(test, value, nums, p2flag=False):
-    if not nums or value > test:
+def calibrate(test, value, i, nums, p2flag=False):
+    if i == len(nums) or value > test:
         return value == test, p2flag
 
     for op in (add, mul, concat):
-        result = calibrate(test, op(value, nums[0]), nums[1:], (p2flag or op == concat))
+        result = calibrate(test, op(value, nums[i]), i+1, nums, (p2flag or op == concat))
         if result[0]:
             return result
 
@@ -24,7 +24,7 @@ with open(sys.argv[1] if len(sys.argv) > 1 else '../input/07') as f:
         test = int(test)
         nums = tuple(map(int, nums.split()))
 
-        result, p2flag = calibrate(test, nums[0], nums[1:])
+        result, p2flag = calibrate(test, nums[0], 1, nums)
 
         p1 += test*(result and not p2flag)
         p2 += test*result
