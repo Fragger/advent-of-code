c01e762076e9f95af8be60e408a0569884b6b418
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-09 22:49:42 -0800

    nit 2024 day 10, change loc for unpacked x, y

--- 2024/solution/10.py
+++ 2024/solution/10.py
@@ -4,16 +4,16 @@ from collections import Counter
 with open(sys.argv[1] if len(sys.argv) > 1 else '../input/10') as f:
     topo = {(x, y): int(h) for y, line in enumerate(f.read().splitlines()) for x, h in enumerate(line)}
 
-def step(ans, h, loc):
-    if loc in topo and h == topo[loc]:
+def step(ans, h, x, y):
+    if (x, y) in topo and h == topo[x, y]:
         if h == 9:
-            ans[loc] += 1
+            ans[x, y] += 1
         else:
-            for x, y in ((0, -1), (1, 0), (0, 1), (-1, 0)):
-                step(ans, h+1, (loc[0]+x, loc[1]+y))
+            for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)):
+                step(ans, h+1, x+dx, y+dy)
     return ans
 
-trails = [step(Counter(), 0, loc) for loc, h in topo.items() if h == 0]
+trails = [step(Counter(), 0, x, y) for (x, y), h in topo.items() if h == 0]
 
 print(sum(len(trail) for trail in trails))
 print(sum(trail.total() for trail in trails))
