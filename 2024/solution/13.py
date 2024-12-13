d7a29bedfbeef99efa802c2de3ab37d9e7bb7e05
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-12 22:36:19 -0800

    Solution for 2024 day 13

---
+++ 2024/solution/13.py
@@ -0,0 +1,28 @@
+import sys
+
+def calc(ax, ay, bx, by, prizex, prizey):
+    #ax*i+bx*j=prizex
+    #ay*i+by*j=prizey
+    det = ax*by-bx*ay
+
+    i = round((by*prizex-bx*prizey)/det)
+    j = round((-ay*prizex+ax*prizey)/det)
+
+    if ax*i+bx*j == prizex and ay*i+by*j == prizey:
+        return i*3+j
+    return 0
+
+p1 = 0
+p2 = 0
+with open(sys.argv[1] if len(sys.argv) > 1 else '../input/13') as f:
+    for machine in f.read().split('\n\n'):
+        (ax, ay), (bx, by), (prizex, prizey) = ((int(n.strip()[2:])
+                                                 for n in line.split(':')[1].split(','))
+                                                for line in machine.splitlines())
+        p1 += calc(ax, ay, bx, by, prizex, prizey)
+
+        prizex += 10000000000000
+        prizey += 10000000000000
+        p2 += calc(ax, ay, bx, by, prizex, prizey)
+print(p1)
+print(p2)
