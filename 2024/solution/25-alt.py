72a9b772c2564fe1085ea2da2e5cc99f42bbbfff
Kevin Brasier <wonderfulweb@gmail.com>
2024-12-25 00:02:25 -0800

    nit 2024 day 25-alt, use enumerate for parsing

--- 2024/solution/25-alt.py
+++ 2024/solution/25-alt.py
@@ -1,10 +1,8 @@
 import sys
 from itertools import combinations
 
-schematics = []
 with open(sys.argv[1] if len(sys.argv) > 1 else '../input/25') as f:
-    for sch in f.read().split('\n\n'):
-        sch = list(sch.splitlines())
-        schematics.append({(x, y) for y in range(len(sch)) for x in range(len(sch[0])) if sch[y][x] == '#'})
+    schematics = [{(x, y) for y, line in enumerate(sch.splitlines()) for x, c in enumerate(line) if c == '#'}
+                  for sch in f.read().split('\n\n')]
 
 print(sum(a.isdisjoint(b) for a, b in combinations(schematics, 2)))
