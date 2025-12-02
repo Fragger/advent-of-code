f5c113e7b6e6c3f175ebaf9e8641bcf5989f5f1c
Kevin Brasier <wonderfulweb@gmail.com>
2025-12-02 02:32:54 -0800

    Solution for 2025 day 02

---
+++ 2025/solution/02.py
@@ -0,0 +1,22 @@
+import sys
+
+p1 = 0
+p2 = 0
+with open(sys.argv[1] if len(sys.argv) > 1 else "../input/02") as f:
+    for first, last in (idrange.split("-") for idrange in f.read().split(",")):
+        for num in range(int(first), int(last) + 1):
+            snum = str(num)
+            num_len = len(snum)
+            for size in reversed(range(1, num_len // 2 + 1)):
+                if num_len % size == 0:
+                    for i in range(size, num_len, size):
+                        if snum[:size] != snum[i : i + size]:
+                            break
+                    else:
+                        if size == num_len / 2:
+                            p1 += num
+                        p2 += num
+                        break
+
+print(p1)
+print(p2)
