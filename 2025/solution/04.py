bf425e0beeb37113a06bf0e3a409f7c29835a962
Kevin Brasier <wonderfulweb@gmail.com>
2025-12-03 22:21:13 -0800

    Update 2025 day 04 to use a set instead of dict

--- 2025/solution/04.py
+++ 2025/solution/04.py
@@ -1,31 +1,26 @@
 import sys
+from itertools import product
 
 with open(sys.argv[1] if len(sys.argv) > 1 else "../input/04") as f:
     grid = {
-        (x, y): char
+        (x, y)
         for y, line in enumerate(f.read().splitlines())
         for x, char in enumerate(line)
+        if char == "@"
     }
 
 p1 = 0
-removed = set()
-toremove = set()
-while toremove or not p1:
-    toremove = set()
-    for (x, y), char in grid.items():
-        if char == "@" and (x, y) not in removed:
-            if (
-                sum(
-                    grid.get((x + i, y + j)) == "@" and (x + i, y + j) not in removed
-                    for i in (-1, 0, 1)
-                    for j in (-1, 0, 1)
-                )
-                <= 4
-            ):
-                toremove.add((x, y))
-    if not removed:
-        p1 = len(toremove)
-    removed.update(toremove)
+p2 = 0
+remove = set()
+while remove or not p1:
+    remove = set()
+    for x, y in grid:
+        if sum((x + i, y + j) in grid for i, j in product((-1, 0, 1), repeat=2)) <= 4:
+            remove.add((x, y))
+    if not p1:
+        p1 = len(remove)
+    p2 += len(remove)
+    grid -= remove
 
 print(p1)
-print(len(removed))
+print(p2)
