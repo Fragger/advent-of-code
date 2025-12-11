e0e1aec5e1be8f95765a4e4ef69929738bc7e2f5
Kevin Brasier <wonderfulweb@gmail.com>
2025-12-11 01:32:18 -0800

    Update 2025 day 11 to make trace more generic

--- 2025/solution/11.py
+++ 2025/solution/11.py
@@ -5,14 +5,11 @@ cables = {}
 
 
 @cache
-def trace(dev, dac=False, fft=False):
+def trace(dev, *visit):
     if dev == "out":
-        return dac and fft
-    if dev == "dac":
-        dac = True
-    elif dev == "fft":
-        fft = True
-    return sum(trace(path, dac, fft) for path in cables[dev])
+        return len(visit) == 0
+    visit = [vdev for vdev in visit if vdev != dev]
+    return sum(trace(path, *visit) for path in cables[dev])
 
 
 with open(sys.argv[1] if len(sys.argv) > 1 else "../input/11") as f:
@@ -20,5 +17,5 @@ with open(sys.argv[1] if len(sys.argv) > 1 else "../input/11") as f:
         dev, outs = line.split(": ")
         cables[dev] = outs.split()
 
-print(trace("you", True, True))
-print(trace("svr"))
+print(trace("you"))
+print(trace("svr", "dac", "fft"))
